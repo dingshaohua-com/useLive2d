659d32c83d386482543fe585199f459c12f2b42b
pigeoner <2390730311@qq.com>
2021-12-16 14:11:39 +0800

    修正ajax加载json的错误

--- useLive2d/live2dCfg/waifu-tips.js
+++ useLive2d/live2dCfg/waifu-tips.js
@@ -3,7 +3,7 @@ function render(template, context) {
     var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
 
     return template.replace(tokenReg, function (word, slash1, token, slash2) {
-        if (slash1 || slash2) {  
+        if (slash1 || slash2) {
             return word.replace('\\', '');
         }
 
@@ -25,54 +25,54 @@ String.prototype.render = function (context) {
 
 var re = /x/;
 console.log(re);
-re.toString = function() {
+re.toString = function () {
     showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000);
     return '';
 };
 
-$(document).on('copy', function (){
+$(document).on('copy', function () {
     showMessage('你都复制了些什么呀，转载要记得加上出处哦', 5000);
 });
 
 $.ajax({
     cache: true,
-    url: `${use2dParam.path}/live2dCfg/waifu-tips.js`,
+    url: `${use2dParam.path}/live2dCfg/waifu-tips.json`,
     dataType: "json",
-    success: function (result){
-        $.each(result.mouseover, function (index, tips){
-            $(document).on("mouseover", tips.selector, function (){
+    success: function (result) {
+        $.each(result.mouseover, function (index, tips) {
+            $(document).on("mouseover", tips.selector, function () {
                 var text = tips.text;
-                if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                text = text.render({text: $(this).text()});
+                if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+                text = text.render({ text: $(this).text() });
                 showMessage(text, 3000);
             });
         });
-        $.each(result.click, function (index, tips){
-            $(document).on("click", tips.selector, function (){
+        $.each(result.click, function (index, tips) {
+            $(document).on("click", tips.selector, function () {
                 var text = tips.text;
-                if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                text = text.render({text: $(this).text()});
+                if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+                text = text.render({ text: $(this).text() });
                 showMessage(text, 3000);
             });
         });
     }
 });
 
-(function (){
+(function () {
     var text;
-    if(document.referrer !== ''){
+    if (document.referrer !== '') {
         var referrer = document.createElement('a');
         referrer.href = document.referrer;
         text = 'Hello! 来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友';
         var domain = referrer.hostname.split('.')[1];
         if (domain == 'baidu') {
             text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
-        }else if (domain == 'so') {
+        } else if (domain == 'so') {
             text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
-        }else if (domain == 'google') {
+        } else if (domain == 'google') {
             text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
         }
-    }else {
+    } else {
         if (window.location.href == 'https://imjad.cn/') { //如果是主页
             var now = (new Date()).getHours();
             if (now > 23 || now <= 5) {
@@ -94,31 +94,31 @@ $.ajax({
             } else {
                 text = '嗨~ 快来逗我玩吧！';
             }
-        }else {
+        } else {
             text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
         }
     }
     showMessage(text, 6000);
 })();
 
-window.setInterval(showHitokoto,30000);
+window.setInterval(showHitokoto, 30000);
 
-function showHitokoto(){
-    $.getJSON('https://api.imjad.cn/hitokoto/?cat=&charset=utf-8&length=28&encode=json',function(result){
+function showHitokoto() {
+    $.getJSON('https://api.imjad.cn/hitokoto/?cat=&charset=utf-8&length=28&encode=json', function (result) {
         showMessage(result.hitokoto, 5000);
     });
 }
 
-function showMessage(text, timeout){
-    if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
+function showMessage(text, timeout) {
+    if (Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1) - 1];
     console.log(text);
     $('.waifu-tips').stop();
     $('.waifu-tips').html(text).fadeTo(200, 1);
     if (timeout === null) timeout = 5000;
     hideMessage(timeout);
 }
-function hideMessage(timeout){
-    $('.waifu-tips').stop().css('opacity',1);
+function hideMessage(timeout) {
+    $('.waifu-tips').stop().css('opacity', 1);
     if (timeout === null) timeout = 5000;
     $('.waifu-tips').delay(timeout).fadeTo(200, 0);
 }
